8fb242e5319f1f594d7a98fb50c50a4a15b05c83
miguel <mallquilm@gmail.com>
2026-02-14 16:02:21 -0500

    1.3 ciclo de vida spring

--- src/main/java/edu/pe/cibertec/beans/DocumentoExcel.java
+++ src/main/java/edu/pe/cibertec/beans/DocumentoExcel.java
@@ -1,6 +1,15 @@
 package edu.pe.cibertec.beans;
 
 public class DocumentoExcel implements Imprimible{
+
+    private void iniciar(){
+        System.out.println("Iniciando la creación de la instancia bean");
+    }
+
+    private void destruir(){
+        System.out.println("destruyendo instancia creada ");
+    }
+
     @Override
     public String imprimir() {
         return "Imprimiendo desde un archivo Excel.";
