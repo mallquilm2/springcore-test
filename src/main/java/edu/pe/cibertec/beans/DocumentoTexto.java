29cfa4d83cb0d0bf14ef0032a8aacb302785147d
miguel <mallquilm@gmail.com>
2026-02-14 16:13:45 -0500

    1.4 tipos de inicialización

--- src/main/java/edu/pe/cibertec/beans/DocumentoTexto.java
+++ src/main/java/edu/pe/cibertec/beans/DocumentoTexto.java
@@ -2,6 +2,14 @@ package edu.pe.cibertec.beans;
 
 public class DocumentoTexto  implements Imprimible{
 
+    private void iniciar(){
+        System.out.println("Iniciando la creación de la instancia del bean de TEXTO");
+    }
+
+    private void destruir(){
+        System.out.println("Destruyendo la instancia del bean de TEXTO");
+    }
+
     @Override
     public String imprimir() {
         return "Imprimiendo desde un archivo de texto.";
